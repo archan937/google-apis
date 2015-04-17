3680a9f3a10b0b55259af9e81afeb5fc8e28b39b
Paul Engel <pm_engel@icloud.com>
2015-04-17 16:48:39 +0200

    Added Google Storage API definition

---
+++ lib/google_apis/api/storage.rb
@@ -0,0 +1,12 @@
+module GoogleApis
+  class Api
+    class Storage
+      extend Base
+
+      api "storage"
+      version 1
+      auth_scope "https://www.googleapis.com/auth/devstorage.read_write"
+
+    end
+  end
+end
